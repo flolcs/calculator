f507ec8f92fdf0ddb94427b98eddd12da736e94a
FloLcs <florianlcs16@gmail.com>
2022-05-30 11:48:44 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const buttons = document.querySelectorAll(".case");
+const result = document.getElementById("result");
+const egal = document.querySelector(".egal");
+const reset = document.querySelector(".reset");
