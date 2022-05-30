93fcbc6fcde130a4431a9e89277eadcce3af28c6
FloLcs <florianlcs16@gmail.com>
2022-05-30 13:47:46 +0200

    complet sauf virgule

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
-const buttons = document.querySelectorAll(".case");
+const btn = document.querySelectorAll(".case");
 const result = document.getElementById("result");
-const egal = document.querySelector(".egal");
-const reset = document.querySelector(".reset");
+const egal = document.getElementById("egal");
+const reset = document.getElementById("reset");
+
+console.log(btn);
+
+reset.addEventListener("click", () => {
+  result.textContent = "";
+});
+
+egal.addEventListener("click", () => {
+  result.textContent = eval(result.textContent);
+});
+
+btn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    result.textContent += e.target.id;
+  });
+});
